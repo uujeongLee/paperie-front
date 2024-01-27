1e37f0f5c410628be3714bf49253813063cc3943
uujeongLee <uujeong59@sookmyung.ac.kr>
2024-01-27 18:33:15 +0900

    사이드바 길이 수정 및 reference.js 투명도 수정

--- src/components/References.js
+++ src/components/References.js
@@ -11,7 +11,7 @@ function References() {
     const handleScroll = () => {
       // 스크롤 위치에 따라 애니메이션 컨트롤
       const scrollY = window.scrollY;
-      controls.start({ opacity: scrollY > 50 ? 1 : 0.5 }); // 스크롤 위치에 따라 opacity 조절
+      controls.start({ opacity: scrollY > 400 ? 1 : 0.1 }); // 스크롤 위치에 따라 opacity 조절
     };
 
     // 스크롤 이벤트 리스너 추가
@@ -34,9 +34,9 @@ function References() {
 
   return (
     <motion.div
-    initial={{ opacity: 0.5 }}
+    initial={{ opacity: 0.0 }}
     animate={controls}
-    transition={{ ease: "easeInOut", duration: 0.5 }}
+    transition={{ ease: "easeInOut", duration: 0.2 }}
     className="table_border"
   >
     <table className="caption-top table-borderless table-hover">
